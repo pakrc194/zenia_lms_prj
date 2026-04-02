8c7efaf9c49511f7449dc4eb2ea6325fd6e20096
pakrc194 <pakrc194@gmail.com>
2026-04-02 09:14:49 +0900

    commit set QueryDSL2

--- backend/src/main/java/com/example/lms/config/CorsConfiguration.java
+++ backend/src/main/java/com/example/lms/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.lms.cors;
+package com.example.lms.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
